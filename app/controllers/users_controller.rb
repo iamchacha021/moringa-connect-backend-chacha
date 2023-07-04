16a5f085d1dd3d0573d8b1d173d52d29e6b93fe7
iamchacha021 <iamchacha021@gmail.com>
2023-07-04 23:27:32 +0300

    1. Create sessions controller 2. Create JWT authentication in the backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,9 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
+    @user = User.create!(user_params)
+    if @user.valid?
+      render json: @user, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +45,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :cohort, :email, :password_digest, :is_admin, :is_alumni)
+      params.permit(:first_name, :last_name, :cohort, :email, :password, :is_admin, :is_alumni)
     end
 end
