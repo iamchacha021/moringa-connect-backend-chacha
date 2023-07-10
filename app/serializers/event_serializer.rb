5c71e611302a4ad687860b4d741ac389c9e48ed4
iamchacha021 <iamchacha021@gmail.com>
2023-07-10 19:52:41 +0300

    Change the time to be displayed as HH:MM a.m/p.m

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,7 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image_url, :date, :time, :venue
+  attributes :id, :name, :description, :image_url, :date, :formatted_time, :venue
+
+  def formatted_time
+    object.time.strftime("%I:%M %p")
+  end
 end
